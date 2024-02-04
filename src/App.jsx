d32e70679f36e146b1cd2fb19c1a1188891481f1
Luis Mendieta <luismepa@gmail.com>
2024-02-04 16:06:04 -0600

    ui: add layout

--- src/App.jsx
+++ src/App.jsx
@@ -10,16 +10,22 @@ import SkillsSection from './components/skills-section';
 
 function App() {
   return (
-    <div className="container mx-auto px-2 sm:px-0">
-      <FullBackground />
-      <ProfileCard />
-      <IntroduceSection />
-      <AboutSection />
-      <ResumeSection />
-      <SkillsSection />
-      <PortfolioSection />
-      <ContactSection />
-      <div className="space my-20"></div>
+    <div className="min-h-screen flex flex-col gap-20 mt-10 xl:flex-row">
+      <header className="xl:w-96">
+        <div className="xl:fixed xl:h-screen xl:top-0 xl:bottom-0 xl:left-0 flex items-center justify-center">
+          <ProfileCard />
+        </div>
+      </header>
+      <main className="flex flex-col gap-12 max-w-3xl mx-auto px-3 xl:px-0">
+        <FullBackground />
+        <IntroduceSection />
+        <AboutSection />
+        <ResumeSection />
+        <SkillsSection />
+        <PortfolioSection />
+        <ContactSection />
+        <div className="space my-20"></div>
+      </main>
     </div>
   );
 }
